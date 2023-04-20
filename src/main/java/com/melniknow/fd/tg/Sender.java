d9241b3dcbd32e99df23f6b654adf1b4fe129322
Sergey Melnikov <smelnikovimit@gmail.com>
2023-04-20 10:28:15 +0600

    [+] Add Sender API

--- src/main/java/com/melniknow/fd/tg/Sender.java
+++
@@ -1,9 +0,0 @@
-package com.melniknow.fd.tg;
-
-import com.melniknow.fd.core.MathUtils;
-
-public class Sender {
-    public static void send(MathUtils.CalculatedFork fork) {
-        System.out.println(fork);
-    }
-}
