b2439dacfb64a0662d4bb5be720d9f854ba9e96e
arkadii_rudenko <rudenko.arkashaa@gmail.com>
2023-04-21 09:32:19 +0600

    [*] fix GET request

--- src/main/java/com/melniknow/fd/oddscorp/Parser.java
+++ src/main/java/com/melniknow/fd/oddscorp/Parser.java
@@ -7,13 +7,15 @@ import com.google.gson.JsonParser;
 import com.melniknow.fd.context.Context;
 import com.melniknow.fd.core.Logger;
 import io.mikael.urlbuilder.UrlBuilder;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +32,7 @@ public class Parser {
                        String bkName2, String event2, BetType type2, String link2,
                        BigDecimal ratio2, String bet2) { }
 
+    private static final CloseableHttpClient httpClient = HttpClients.createDefault();
 
     public static List<Fork> getForks(ParserParams params) {
         var uri = UrlBuilder.fromString("http://api.oddscp.com:8111/forks")
@@ -45,19 +48,18 @@ public class Parser {
 
         var stringForks = FakeServer.get(uri.toString());
 
-        try {
-            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            var br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.US_ASCII));
-            var sb = new StringBuilder();
-            String output;
-            while ((output = br.readLine()) != null) {
-                sb.append(output);
+        HttpGet request = new HttpGet(uri);
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            if (response.getStatusLine().getStatusCode() != 200) {
+                return null;
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                stringForks = EntityUtils.toString(entity); // GetBody
+//                Logger.writeToLogSession(stringForks);
+            }
+        } catch (IOException ignored) {
+
         }
 
         var jsonParser = JsonParser.parseString(stringForks);
