5d0fb5a41e3464a6bde0350bb979d9dcb3278653
Sergey Melnikov <smelnikovimit@gmail.com>
2023-04-21 15:42:05 +0600

    [*] Some refactor

--- src/main/java/com/melniknow/fd/tg/Sender.java
+++ src/main/java/com/melniknow/fd/tg/Sender.java
@@ -27,25 +27,25 @@ public class Sender {
     private static String getDataMessage(BetsUtils.CompleteBetsFork completedFork) {
         var fork = completedFork.calculatedFork().fork();
         return String.format(
-                "Поставлена вилка! " + "\u26A1" + "\u26A1" + "\u26A1" +  "\n\n" +
-                "<i>Спорт:</i> <b>" + fork.sport() +                     "</b>" + "\n" +
-                "<i>Тип ставки:</i> <b>" + fork.betType() +              "</b>" + "\n\n" +
+            "Поставлена вилка! " + "\u26A1" + "\u26A1" + "\u26A1" + "\n\n" +
+                "<i>Спорт:</i> <b>" + fork.sport() + "</b>" + "\n" +
+                "<i>Тип ставки:</i> <b>" + fork.betType() + "</b>" + "\n\n" +
 
                 "Букмекер 1\n" +
-                "<i>Имя:</i> <b>" + fork.bkName1() +                     "</b>" + "\n" +
-                "<i>Событие:</i> <b>" + fork.event1() +                  "</b>" + "\n" +
-                "<i>Тип ставки:</i> <b>" + fork.bet1() +                 "</b>" + "\n" +
-                "<i>Ссылка:</i> <b>" + fork.link1() +                    "</b>" + "\n" +
-                "<i>Коэффициент:</i> <b>" + fork.ratio1() +              "</b>" + "\n" +
-                "<i>Тип ставки:</i> <b>" + fork.type1() +                "</b>" + "\n\n" +
+                "<i>Имя:</i> <b>" + fork.bkName1() + "</b>" + "\n" +
+                "<i>Событие:</i> <b>" + fork.event1() + "</b>" + "\n" +
+                "<i>Тип ставки:</i> <b>" + fork.bet1() + "</b>" + "\n" +
+                "<i>Ссылка:</i> <b>" + fork.link1() + "</b>" + "\n" +
+                "<i>Коэффициент:</i> <b>" + fork.ratio1() + "</b>" + "\n" +
+                "<i>Тип ставки:</i> <b>" + fork.type1() + "</b>" + "\n\n" +
 
                 "Букмекер 2\n" +
-                "<i>Имя:</i> <b>" + fork.bkName2() +                     "</b>" + "\n" +
-                "<i>Событие:</i> <b>" + fork.event2() +                  "</b>" + "\n" +
-                "<i>Тип ставки:</i> <b>" + fork.bet2() +                 "</b>" + "\n" +
-                "<i>Ссылка:</i> <b>" + fork.link2() +                    "</b>" + "\n" +
-                "<i>Коэффициент:</i> <b>" + fork.ratio2() +              "</b>" + "\n" +
-                "<i>Тип ставки:</i> <b>" + fork.type2() +                "</b>" + "\n\n" +
+                "<i>Имя:</i> <b>" + fork.bkName2() + "</b>" + "\n" +
+                "<i>Событие:</i> <b>" + fork.event2() + "</b>" + "\n" +
+                "<i>Тип ставки:</i> <b>" + fork.bet2() + "</b>" + "\n" +
+                "<i>Ссылка:</i> <b>" + fork.link2() + "</b>" + "\n" +
+                "<i>Коэффициент:</i> <b>" + fork.ratio2() + "</b>" + "\n" +
+                "<i>Тип ставки:</i> <b>" + fork.type2() + "</b>" + "\n\n" +
 
                 "<i>Информация по вилке:</i> <b>" + completedFork.info() + "</b>" + "\n",
             StandardCharsets.UTF_8
