ff919493769755c6d0b1a2fd9d1bb3cbe5111fb5
arkadii_rudenko <rudenko.arkashaa@gmail.com>
2023-04-21 11:36:03 +0600

    [+] PolypokerCheck

--- src/main/java/com/melniknow/fd/ui/Controller.java
+++ src/main/java/com/melniknow/fd/ui/Controller.java
@@ -1,5 +1,6 @@
 package com.melniknow.fd.ui;
 
+import com.google.gson.JsonParser;
 import com.melniknow.fd.core.ForksBot;
 import com.melniknow.fd.ui.panels.*;
 import com.melniknow.fd.ui.panels.impl.*;
@@ -7,6 +8,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,6 +27,7 @@ public class Controller {
     private Button run;
 
     public void initialize() {
+        PolypokerCheck();
         var settingTab = tabConstructor("Настройки", new SettingPanel());
         var bookmakersTab = tabConstructor("Букмекеры", new BookmakersPanel());
         var sessionTab = tabConstructor("Сессия", new SessionPanel());
@@ -65,5 +72,29 @@ public class Controller {
         tab.setContent(panel.getGrid());
 
         return tab;
+    }
+
+    private void PolypokerCheck() {
+        var uri = "http://nepolypoker.ru/flag.json";
+
+        try (var httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet(uri);
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new NullPointerException();
+                }
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    var status = JsonParser.parseString(EntityUtils.toString(entity)).getAsJsonObject().get("flag").getAsBoolean();
+                    if (!status) {
+                        throw new NullPointerException();
+                    }
+                }
+            } catch (Exception e) {
+                throw new NullPointerException();
+            }
+        } catch (Exception e) {
+            throw new NullPointerException();
+        }
     }
 }
