c2c0b89c2a5ef35209a9cbe0f7cabc707faffc82
Sergey Melnikov <smelnikovimit@gmail.com>
2023-04-22 17:39:08 +0600

    [+] Add screens manage2r

--- src/main/java/com/melniknow/fd/selenium/ScreensManager.java
+++ src/main/java/com/melniknow/fd/selenium/ScreensManager.java
@@ -4,17 +4,26 @@ import com.melniknow.fd.oddscorp.Bookmakers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.HashMap;
+
 public class ScreensManager {
     private static final ChromeDriver driver;
     static {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
-    public synchronized void createScreenForBookmaker(Bookmakers bookmaker) {
 
+    private final HashMap<Bookmakers, String> screenStorage = new HashMap<>();
+
+    public synchronized void createScreenForBookmaker(Bookmakers bookmaker) {
+        screenStorage.put(bookmaker, "");
     }
 
     public synchronized void removeScreenForBookmaker(Bookmakers bookmaker) {
+        screenStorage.remove(bookmaker);
+    }
 
+    public synchronized String getScreenForBookmaker(Bookmakers bookmaker) {
+        return screenStorage.get(bookmaker);
     }
 }
