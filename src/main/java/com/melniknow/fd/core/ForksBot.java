cb64634dc49e1fc2b61ed305f8bb07eb8953b726
arkadii_rudenko <rudenko.arkashaa@gmail.com>
2023-04-22 12:24:07 +0600

    [*] just

--- src/main/java/com/melniknow/fd/core/ForksBot.java
+++ src/main/java/com/melniknow/fd/core/ForksBot.java
@@ -11,9 +11,9 @@ public class ForksBot implements Runnable {
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.MILLISECONDS.sleep(500);
 
-                var forks = Parser.getForks(Context.parserParams);
+                var forksz = Parser.getForks(Context.parserParams);
                 var calculated = MathUtils.calculate(forks);
 
                 if (calculated != null) {
