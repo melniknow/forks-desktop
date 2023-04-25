e39e9e96076861043cc78316575429037b400157
Sergey Melnikov <smelnikovimit@gmail.com>
2023-04-25 14:42:01 +0600

    [*] in work

--- src/main/java/com/melniknow/fd/betting/ScreenManager.java
+++ src/main/java/com/melniknow/fd/betting/ScreenManager.java
@@ -27,24 +27,34 @@ public class ScreenManager {
         var params = Context.betsParams.get(bookmaker);
         var link = params.link();
 
-//        var proxy = new Proxy();
-//        proxy.setHttpProxy("102.165.51.172:3128");
-
-        var dimension = new Dimension(800, 800);
-
         var options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-//        options.setCapability("proxy", proxy);
+
+        if (!params.proxyIp().isEmpty()) {
+            options.addArguments("--proxy-server=socks5://" + "62.113.105.132" + ":" + "40171");
+            options.addArguments("--proxy-auth=" + "5lfnqi" + ":" + "dsxozk");
+//            var proxy = new Proxy();
+//            proxy.setSocksProxy("5lfnqi:dsxozk@62.113.105.132:40171");
+//            proxy.setSocksVersion(5);
+//            proxy.setAutodetect(false);
+//            options.setProxy(proxy);
+//            options.addArguments("ignore-certificate-errors");
+        }
 
         var driver = new ChromeDriver(options);
+
+        var dimension = new Dimension(800, 800);
         driver.manage().window().setSize(dimension);
 
         screenStorage.put(bookmaker, driver);
-        try {
-            driver.get("https://google.com"); // link
-        } catch (Exception e) {
-            Logger.writeToLogSession("Бот не смог открыть ссылку - " + link);
-        }
+
+        Context.parsingPool.execute(() -> {
+            try {
+                driver.get("https://2ip.ru"); // link
+            } catch (Exception e) {
+                Logger.writeToLogSession("Бот не смог открыть ссылку - " + link);
+            }
+        });
     }
 
     public synchronized void removeScreenForBookmaker(Bookmaker bookmaker) {
