0d801564c63e69305ea20329f8328a8993416c2e
arkadii_rudenko <rudenko.arkashaa@gmail.com>
2023-04-28 13:45:53 +0600

    [*] fix _188Bet

--- src/main/java/com/melniknow/fd/betting/bookmakers/impl/_188Bet.java
+++ src/main/java/com/melniknow/fd/betting/bookmakers/impl/_188Bet.java
@@ -2,14 +2,9 @@ package com.melniknow.fd.betting.bookmakers.impl;
 
 import com.melniknow.fd.Context;
 import com.melniknow.fd.betting.bookmakers.IBookmaker;
-import com.melniknow.fd.betting.utils._188bet.basketball.BasketballHandicap;
-import com.melniknow.fd.betting.utils._188bet.basketball.BasketballTotals;
-import com.melniknow.fd.betting.utils._188bet.basketball.BasketballWin;
 import com.melniknow.fd.betting.utils._188bet.TotalSportHandicap;
 import com.melniknow.fd.betting.utils._188bet.TotalSportsTotals;
 import com.melniknow.fd.betting.utils._188bet.TotalsSportsWin;
-import com.melniknow.fd.betting.utils._188bet.tennis.TennisTotals;
-import com.melniknow.fd.betting.utils._188bet.tennis.TennisWin;
 import com.melniknow.fd.core.Parser;
 import com.melniknow.fd.domain.Bookmaker;
 import com.melniknow.fd.domain.Sports;
@@ -23,32 +18,17 @@ public class _188Bet implements IBookmaker {
 
     @Override
     public void clickOnBetType(Bookmaker bookmaker, Parser.BetInfo info, Sports sport) {
-        switch (sport) {
-            case SOCCER -> {
-                switch (info.BK_bet_type()) {
-                    case WIN -> TotalsSportsWin.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case TOTALS -> TotalSportsTotals.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case HANDICAP -> TotalSportHandicap.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    default -> throw new RuntimeException("BetType`s not supported");
-                }
-            } case TENNIS -> {
-                switch (info.BK_bet_type()) {
-                    case WIN -> TennisWin.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case TOTALS -> TennisTotals.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case HANDICAP -> TotalSportHandicap.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    default -> throw new RuntimeException("BetType`s not supported");
-                }
-
-            } case BASKETBALL -> {
-                switch (info.BK_bet_type()) {
-                    case WIN -> BasketballWin.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case TOTALS -> BasketballTotals.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    case HANDICAP -> BasketballHandicap.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
-                    default -> throw new RuntimeException("BetType`s not supported");
-                }
+        if (sport.equals(Sports.BASKETBALL) || sport.equals(Sports.SOCCER) || sport.equals(Sports.TENNIS))
+            switch (info.BK_bet_type()) {
+                case WIN ->
+                    TotalsSportsWin.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
+                case TOTALS ->
+                    TotalSportsTotals.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
+                case HANDICAP ->
+                    TotalSportHandicap.click(Context.screenManager.getScreenForBookmaker(bookmaker), info);
+                default -> throw new RuntimeException("BetType`s not supported");
             }
-            default -> throw new RuntimeException("Sport`s not supported");
-        }
+
     }
 
     @Override
